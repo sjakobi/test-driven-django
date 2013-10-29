4adec92f5d83b256aacbc5879aade36e9bb3c12e
sjakobi <simon.jakobi@gmail.com>
2013-10-30 00:59:41 +0100

    FT now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -13,10 +14,43 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
 
         self.browser.get("http:localhost:8000")
-
+        
+        # Correct page title and header
         self.assertIn("To-Do", self.browser.title)
-        self.fail("Write user story as comments!")
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn("To-Do", header_text)
+
+        # Can enter to-do item on frontpage
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        self.assertEqual(inputbox.get_attribute("placeholder"),
+                         "Enter a to_do item")
+        
+        # She types "Buy peacock feathers" into a text box
+        inputbox.send_keys("Buy peacock feathers")
+
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id("id_list_table")
+        rows = table.find_elements_by_tag_name("tr")
+        self.assertTrue(
+                any(row.text == "1: Buy peacock feathers" for row in rows))
+
+        # There is still a text box inviting her to add another item.
+        # She enters "Use peacock feathers to make a fly."
         self.fail("Finish the test!")
+
+        # The page updates again, and now shows both items on her list
+
+        # Edith wonders whether the site will remember her list.
+        # She sees that the site has generated a unique URL for her - there is
+        # some explanatory text to that effect.
+
+        # She visits that URL - her to-do list is still there.
+
+        # Satisfied, she goes back to sleep
+
 
 if __name__ == "__main__":
     unittest.main(warnings="ignore")
