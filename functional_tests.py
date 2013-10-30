cb1506cf0b651e332c78e0b4c00fbc05a57fc585
sjakobi <simon.jakobi@gmail.com>
2013-10-30 01:27:15 +0100

    Front page HTML now generated from a template.

--- functional_tests.py
+++ functional_tests.py
@@ -23,7 +23,7 @@ class NewVisitorTest(unittest.TestCase):
         # Can enter to-do item on frontpage
         inputbox = self.browser.find_element_by_id("id_new_item")
         self.assertEqual(inputbox.get_attribute("placeholder"),
-                         "Enter a to_do item")
+                         "Enter a to-do item")
         
         # She types "Buy peacock feathers" into a text box
         inputbox.send_keys("Buy peacock feathers")
@@ -35,7 +35,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id("id_list_table")
         rows = table.find_elements_by_tag_name("tr")
         self.assertTrue(
-                any(row.text == "1: Buy peacock feathers" for row in rows))
+                any(row.text == "1: Buy peacock feathers" for row in rows),
+                "New to-do item did not appear in table")
 
         # There is still a text box inviting her to add another item.
         # She enters "Use peacock feathers to make a fly."
