7482d898d3fe3ff406ee16487340d45d82b98412
sjakobi <simon.jakobi@gmail.com>
2013-11-04 00:58:02 +0100

    Wrapped FTs as app of its own, using Django's LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,9 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -18,7 +19,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
 
-        self.browser.get("http:localhost:8000")
+        self.browser.get(self.live_server_url)
         
         # Correct page title and header
         self.assertIn("To-Do", self.browser.title)
@@ -56,7 +57,3 @@ class NewVisitorTest(unittest.TestCase):
         # She visits that URL - her to-do list is still there.
 
         # Satisfied, she goes back to sleep
-
-
-if __name__ == "__main__":
-    unittest.main(warnings="ignore")
